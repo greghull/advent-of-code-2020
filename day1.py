93c186c7dbb43e8d4193a0d67628a876df87e62d
Greg Hull <greg@cjrc.us>
2020-12-01 18:11:11 -0500

    initial commit

---
+++ day1.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+data = pd.read_csv("input1.txt", header=None)
+sorted = data.squeeze().sort_values().to_numpy()
+
+def day1_part1(array):
+    for i in range(len(array)):
+        for k in range(len(array) - 1, -1, -1):
+            val = array[i] + array[k]
+            if val == 2020:
+                return array[i]*array[k]
+            elif val < 2020:
+                break
+                
+def day1_part2(array):
+    for i in range(len(array)):
+        for j in range(i+1, len(array)):
+            val = array[i] + array[j]
+            if val >= 2020:
+                break
+            for k in range(len(array)):
+                val2 = val + array[k]
+                if val2 > 2020:
+                    break
+                elif val2 == 2020:
+                    return array[i]*array[j]*array[k]
+print(day1_part1(sorted))      
+print(day1_part2(sorted))
