78b65c526bf010c2fd50abfca373b7c8bc247d73
Greg Hull <greg@cjrc.us>
2020-12-02 18:00:00 -0500

    re-factored

--- day2.py
+++ day2.py
@@ -1,33 +1,25 @@
 import re
 
-def part1(filename):
-    def is_valid(min, max, letter, pw):
-        cnt = pw.count(letter)
-        return cnt >= int(min) and cnt <= int(max)
+def parse(line):
+    m = re.match('\A(\d+)-(\d+)\s+(\w+):\s+(\w+)', line)
+    return [m.group(1), m.group(2), m.group(3), m.group(4)]
 
+def test1(min, max, letter, pw):
+    cnt = pw.count(letter)
+    return cnt >= int(min) and cnt <= int(max)
+
+def test2(min, max, letter, pw):
+    return (pw[int(min)-1] == letter) != (pw[int(max)-1] == letter)
+
+def main(filename, test):
     num_valid = 0
 
     with open(filename) as f:
         for line in f:
-            m = re.match('\A(\d+)-(\d+)\s+(\w+):\s+(\w+)', line)
-            if is_valid(min=m.group(1), max=m.group(2), letter=m.group(3), pw=m.group(4)):
+            if test(*parse(line)):
                 num_valid += 1
 
     return num_valid
 
-def part2(filename):
-    def is_valid(min, max, letter, pw):
-        return (pw[int(min)-1] == letter) != (pw[int(max)-1] == letter)
-
-    num_valid = 0
-
-    with open(filename) as f:
-        for line in f:
-            m = re.match('\A(\d+)-(\d+)\s+(\w+):\s+(\w+)', line)
-            if is_valid(min=m.group(1), max=m.group(2), letter=m.group(3), pw=m.group(4)):
-                num_valid += 1
-
-    return num_valid
-
-print(part1('input2.txt'))
-print(part2('input2.txt'))
+print(main('input2.txt', test1))
+print(main('input2.txt', test2))
