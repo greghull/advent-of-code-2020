67d4ffa17e66255a3e9998402f30d3f7bba37bfc
Greg Hull <greg@cjrc.us>
2020-12-11 13:06:41 -0500

    a little cleanup

--- nim/day11.nim
+++ nim/day11.nim
@@ -18,11 +18,10 @@ const
 
 # A rule consists of:
 # - the maximum distance that we should look for empty seats
-#       - None means no maximum distance  (part 2)
+#       - 0 means no maximum distance  (part 2)
 #       - 1 means only look at adjacent seats  (part 1)
 # - the maximum number of seen seats that can be occupied
-    RULES = [(max_distance: 1, max_occupancy: 4), 
-             (max_distance: 0, max_occupancy: 5)]
+    RULES = [(max_distance: 1, max_occupancy: 4), (max_distance: 0, max_occupancy: 5)]
 
 # Given a grid, a starting location, and a direction returns 1 if an occupied seat can be seen in the
 # specified direction.. otherwise returns 0
@@ -30,19 +29,19 @@ const
 # i.e. a max_dist of 1 means adjacent seats only
 proc can_see_occupied(grid: Grid, y: int, x: int, dir: Direction, max_dist=0): int = 
     var 
-        y1 = y + dir[0]
-        x1 = x + dir[1]
+        y = y + dir.north
+        x = x + dir.east
         dist = 1
 
-    while y1 >= 0 and y1<grid.len and x1 >= 0 and x1 < len(grid[y1]):
+    while y >= 0 and y<grid.len and x >= 0 and x < grid[y].len:
         if  max_dist > 0 and dist > max_dist:
             return 0
-        if grid[y1][x1] == '#':
+        if grid[y][x] == '#':
             return 1
-        elif grid[y1][x1] == 'L':
+        elif grid[y][x] == 'L':
             return 0
-        y1 += dir[0]
-        x1 += dir[1]
+        y += dir.north
+        x += dir.east
         dist += 1
 
     return 0
@@ -68,7 +67,6 @@ proc transform_seat(grid: Grid, y: int, x: int, rule: Rule): char =
 
 # Given a grid, transforms the row at index (y) based on the provided rules
 proc transform_row(grid: Grid, y: int, rule: Rule): string = 
-    result = ""
     for x in 0 ..< grid[y].len:
         result &= transform_seat(grid, y, x, rule)
 
@@ -86,7 +84,6 @@ proc num_occupied(grid: Grid): int =
 proc p(grid: Grid) = 
     for line in grid:
         echo line
-    echo ""
 
 # Given a starting grid and a rule, solves the puzzle
 proc solve(input: Grid, rule: Rule): int = 
