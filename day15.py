2b1819f23a096bb2247fc2e1c1cbfcf5881b2292
Greg Hull <greg@cjrc.us>
2020-12-15 10:28:43 -0500

    a bit faster..

--- day15.py
+++ day15.py
@@ -13,5 +13,24 @@ def solve(n_rounds, initial):
 
     return last
 
-print(solve(2020, [0,13,16,17,1,10,6]))
-print(solve(30000000, [0,13,16,17,1,10,6]))
+# Using an array is faster than using a dictionary, but we have to guess blindly at initial size
+def solve2(n_rounds, initial):
+    mem = [None] * 100000000
+    last = 0
+
+    for i, n in enumerate(initial):
+        mem[n] = i+1
+        last = n
+
+    for i in range(len(initial), n_rounds):
+        if not mem[last]:
+            next = 0
+        else:
+            next = i - mem[last]
+        mem[last] = i
+        last = next
+
+    return last
+
+print(solve2(2020, [0,13,16,17,1,10,6]))
+print(solve2(30000000, [0,13,16,17,1,10,6]))
