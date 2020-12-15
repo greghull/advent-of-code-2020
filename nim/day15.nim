90ca2b3cdcdd0b0fdf13a9f6c7f3327233e826dd
Greg Hull <greg@cjrc.us>
2020-12-15 10:28:58 -0500

    much faster.. runs in under 4s

--- nim/day15.nim
+++ nim/day15.nim
@@ -3,15 +3,36 @@ import tables
 proc solve(n_rounds: int, initial: seq[int]): int =
     var
         mem = initTable[int,int]()
+        next = 0
 
     for i in 0 ..< len(initial):
         mem[initial[i]] = i+1
         result = initial[i]
 
     for i in len(initial) ..< n_rounds:
-        let next = i - mem.getOrDefault(result, i)
+        next = i - mem.getOrDefault(result, i)
         mem[result] = i
         result = next
 
-echo solve(2020, @[0,13,16,17,1,10,6])
-echo solve(30000000, @[0,13,16,17,1,10,6])
+# Using an array instead of a Table is __much__ faster
+# but we are taking a blind guess at what the initial array
+# size needs to be
+proc solve2(n_rounds: int, initial: seq[int]): int =
+    var
+        mem = newSeq[int](100000000)
+        next = 0
+
+    for i in 0 ..< len(initial):
+        mem[initial[i]] = i+1
+        result = initial[i]
+
+    for i in len(initial) ..< n_rounds:
+        if mem[result] == 0:
+            next = 0
+        else:
+            next = i - mem[result]
+        mem[result] = i
+        result = next
+
+echo solve2(2020, @[0,13,16,17,1,10,6])
+echo solve2(30000000, @[0,13,16,17,1,10,6])
