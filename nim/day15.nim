191587b8f468a573fdd2544df18310dfce1e9a6c
Greg Hull <greg@cjrc.us>
2020-12-16 13:42:40 -0500

    got rid of table solution

--- nim/day15.nim
+++ nim/day15.nim
@@ -1,23 +1,7 @@
-import tables
-
-proc solve(n_rounds: int, initial: seq[int]): int =
-    var
-        mem = initTable[int,int]()
-        next = 0
-
-    for i in 0 ..< len(initial):
-        mem[initial[i]] = i+1
-        result = initial[i]
-
-    for i in len(initial) ..< n_rounds:
-        next = i - mem.getOrDefault(result, i)
-        mem[result] = i
-        result = next
-
 # Using an array instead of a Table is __much__ faster
 # but we are taking a blind guess at what the initial array
 # size needs to be
-proc solve2(n_rounds: int, initial: seq[int]): int =
+proc solve(n_rounds: int, initial: seq[int]): int =
     var
         mem = newSeq[int](100000000)
         next = 0
@@ -34,5 +18,5 @@ proc solve2(n_rounds: int, initial: seq[int]): int =
         mem[result] = i
         result = next
 
-echo solve2(2020, @[0,13,16,17,1,10,6])
-echo solve2(30000000, @[0,13,16,17,1,10,6])
+echo solve(2020, @[0,13,16,17,1,10,6])
+echo solve(30000000, @[0,13,16,17,1,10,6])
