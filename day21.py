0e582ba0d539b0a85e9383edfb7c19da4d499830
Greg Hull <greg@cjrc.us>
2020-12-22 07:12:20 -0500

    a little clean-up

--- day21.py
+++ day21.py
@@ -1,24 +1,19 @@
-def read(filename):
-    data = []
-    with open(filename) as f:
-        lines = f.read().split('\n')
+def parse_line(line):
+    halves = line.split(' (contains ')
+    halves[0] = set(halves[0].split(' '))
+    if len(halves) > 1:
+        halves[1] = set(halves[1].rstrip(')').split(', '))
+    else:
+        halves[1] = set()
+    return halves
 
-    for line in lines:
-        halves = line.split(' (contains ')
-        halves[0] = set(halves[0].split(' '))
-        if len(halves) > 1:
-            halves[1] = set(halves[1].rstrip(')').split(', '))
-        else:
-            halves[1] = set()
-        data.append(halves)
+with open("input/input21.txt") as f:
+    data =  [parse_line(line) for line in f.read().split('\n')]
 
-    return data
+all_ingredients = set()     # All possible ingredients
+all_allergens = set()       # All possible allergens
+food_map = {}               # A Map from allergens to possibly related ingredients
 
-data = read("input/input21.txt")
-
-all_ingredients = set()
-all_allergens = set()
-food_map = {}
 for d in data:
     all_ingredients |= d[0]
     all_allergens |= d[1]
@@ -30,13 +25,12 @@ for d in data:
     for allergen in d[1]:
         food_map[allergen] &= d[0]
 
-inert_ingredients = all_ingredients.copy()
-
+inert_ingredients = all_ingredients.copy()  # set of ingredients with no allergens
 for allergen, ingredients in food_map.items():
     inert_ingredients.difference_update(ingredients)
 
 ## Part 1 Answer
-print(sum([len(d[0] & inert_ingredients) for d in data]))
+print("P1:", sum([len(d[0] & inert_ingredients) for d in data]))
 
 for i in range(4):
     for allergen, ingredients in food_map.items():
@@ -44,9 +38,6 @@ for i in range(4):
             if allergen != allergen2 and len(ingredients2) == 1:
                 ingredients.difference_update(ingredients2)
 
-
-keys = list(food_map.keys())
-keys.sort()
-print(keys)
 ## Part 2 Answer
-print(",".join([food_map[key].copy().pop() for key in keys]))
+keys = sorted(food_map.keys())
+print("P2:", ",".join([food_map[key].copy().pop() for key in keys]))
